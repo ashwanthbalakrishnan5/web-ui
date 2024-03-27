f3c71266d607384e97276d630da9165c3d191bdc
Ashwanth Balakrishnan <ashwanthbalakrishnan5@gmail.com>
2024-03-27 11:16:24 +0530

    first commit

---
+++ mysite/chat/static/chat/js/script.js
@@ -0,0 +1,17 @@
+const usernameInput1 = document.getElementsById('username');
+const usernameContainer1 = document.getElementById('username-container');
+const mainContent1 = document.getElementById('main-content');
+const labelUsername1 = document.getElementById('label-username');
+
+// btnJoin.addEventListener('click', function() {
+//     // Code to execute when the button is clicked
+//     // const username = usernameInput1.value.trim(); // Trim whitespace
+//   if (username) {
+//     labelUsername1.textContent = username; // Update displayed username
+//     usernameContainer1.style.display = 'none';
+//     mainContent1.style.display = 'block';
+//   } else {
+//     usernameContainer1.style.display = 'block';
+//     mainContent1.style.display = 'none';
+//   }
+//   });
